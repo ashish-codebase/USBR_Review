6d1819acc52d9f547939d148d9d873a104ded8a5
Ashish Masih <masih.ashish@gmail.com>
2025-02-24 01:44:47 -0600

    Initial commit

---
+++ git_clean_sync.bat
@@ -0,0 +1,10 @@
+del .git /F /Q
+git init
+git add .
+git commit -m "Initial commit"
+git remote add origin https://github.com/ashish-codebase/USBR_Review.git
+git branch -M main
+git push --force -u origin main
+git-auto-sync sync
+
+
